27264cb07077e9439525c7e9a909038ab0a220c7
HappyPotatohappy <118255393+HappyPotatohappy@users.noreply.github.com>
2026-08-24 14:56:00 +0900

    [Passed] 블럭쌓는 명령2 (50ms, 16MB)

---
+++ trail2/블럭쌓는 명령2/block-stacking-commands2.py
@@ -0,0 +1,13 @@
+import sys
+input = sys.stdin.readline
+
+n,k = map(int,input().split())
+
+block = [0]*(n+1)
+
+for i in range(k):
+    a,b = map(int,input().split())
+    for j in range(a,b+1):
+        block[j] +=1
+mx = max(block)
+sys.stdout.write(str(mx))
