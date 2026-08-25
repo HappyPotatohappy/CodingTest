df9f842b0223141cc0ce4134a84912d84804d2c1
HappyPotatohappy <118255393+HappyPotatohappy@users.noreply.github.com>
2026-08-25 16:43:26 +0900

    [Passed] 선두를 지켜라 (68ms, 22MB)

--- trail2/선두를 지켜라/keep-the-lead.py
+++ trail2/선두를 지켜라/keep-the-lead.py
@@ -38,20 +38,37 @@ a = fill_list(v,t)
 b = fill_list(v2,t2)
 
 cnt = 0
+now_sd = -1 #선두 변수 0이 a 1이 b -1이면 동률
+
 for i in range(1,tlt+1):
-    if (a[i-1]-b[i-1])*(a[i]-b[i]) < 0:
-        cnt +=1
-    elif (a[i] > b[i] or a[i] < b[i]) and (a[i-1]-b[i-1]) == 0:
-        j = 0
-        while 1:
-            if i-1-j ==0:
-                break
-            if a[i-1-j] != b[i-j-1]:
-                break
-            j += 1
-        if j == 0:
-            continue
-        else:
-            if (a[i-j-1] - b[i-j-1])*(a[i]-b[i]):
-                cnt+=1
+    if b[i] > a[i]:
+        if now_sd == 0:
+            cnt += 1
+        now_sd = 1
+    elif a[i] > b[i] :
+        if now_sd == 1:
+            cnt += 1
+        now_sd = 0
+
 sys.stdout.write(str(cnt))
+
+
+
+
+# for i in range(1,tlt+1):
+#     if (a[i-1]-b[i-1])*(a[i]-b[i]) < 0:
+#         cnt +=1
+#     elif (a[i] > b[i] or a[i] < b[i]) and (a[i-1]-b[i-1]) == 0:
+#         j = 0
+#         while 1:
+#             if i-1-j ==0:
+#                 break
+#             if a[i-1-j] != b[i-j-1]:
+#                 break
+#             j += 1
+#         if j == 0:
+#             continue
+#         else:
+#             if (a[i-j-1] - b[i-j-1])*(a[i]-b[i]):
+#                 cnt+=1
+#sys.stdout.write(str(cnt))
