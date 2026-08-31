e1eb6e636bf7af5ae0f8562cad42cb9314f1e77e
HappyPotatohappy <118255393+HappyPotatohappy@users.noreply.github.com>
2026-08-31 14:41:29 +0900

    [Passed] 한 가지로 열리는 자물쇠 (97ms, 17MB)

---
+++ trail2/한 가지로 열리는 자물쇠/one-way-lock.py
@@ -0,0 +1,14 @@
+import sys
+input = sys.stdin.readline
+N = int(input())
+a, b, c = map(int, input().split())
+
+cnt = 0
+
+for i in range(1,N+1):
+    for j in range(1,N+1):
+        for k in range(1,N+1):
+            if abs(a-i) <= 2 or abs(b-j) <= 2 or abs(c-k) <= 2:
+                cnt += 1
+
+sys.stdout.write(str(cnt))
