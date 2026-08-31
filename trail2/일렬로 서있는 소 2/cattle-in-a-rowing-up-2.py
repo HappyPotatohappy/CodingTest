095a6f8467addae31384b90f2434245d95492d25
HappyPotatohappy <118255393+HappyPotatohappy@users.noreply.github.com>
2026-08-31 10:31:09 +0900

    [Passed] 일렬로 서있는 소 2 (57ms, 17MB)

---
+++ trail2/일렬로 서있는 소 2/cattle-in-a-rowing-up-2.py
@@ -0,0 +1,14 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+A = list(map(int, input().split()))
+
+cnt = 0
+for i in range(N):
+    for j in range(i+1,N):
+        for k in range(j+1,N):
+            if A[i] <= A[j] <= A[k]:
+                cnt += 1
+
+sys.stdout.write(str(cnt))
