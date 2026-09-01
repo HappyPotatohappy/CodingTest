38c8786a5e95ae4db44210e73555bfa1637aa497
HappyPotatohappy <118255393+HappyPotatohappy@users.noreply.github.com>
2026-09-01 22:05:13 +0900

    [Passed] 연속된 정수 만들기 2 (48ms, 16MB)

--- trail2/연속된 정수 만들기 2/create-consecutive-numbers-2.py
+++ trail2/연속된 정수 만들기 2/create-consecutive-numbers-2.py
@@ -1,50 +1,16 @@
 import sys
 input = sys.stdin.readline
-
 pos = list(map(int, input().split()))
 
-#p1, p2, p3
-#이걸 어떻게 옮겨야 하나... 음...
-#p1 을 p2 p3 사이에 음 p1 과 p3중 나머지의 절반 위치쯤 옮겻을 때, 그 값이 작아지는걸 기준으로 해보자.
+pos.sort()
+p1,p2,p3 = pos[0],pos[1],pos[2]
 
-def comp(pos,cnt):
-    p1, p2, p3 = pos[0], pos[1], pos[2]
-    d1 = p2 - p1
-    d2 = p3 - p2
-    if d1 == 1 and d2 ==1:
-        return [1, 1, 1], cnt
-    elif d1 == 2 :
-        cnt+=1
-        p3 = (p1+p2)//2
-        return [p1,p3,p2],cnt
-    elif d2 == 2 :
-        cnt+=1
-        p1 = (p2+p3)//2
-        return [p2,p1,p3],cnt
-    elif d1 > 2 and d2 == 1:
-        p3 = p1 + 2
-        cnt+=1
-        return [p1,p3,p2],cnt
-    elif d2>2 and d1 == 1:
-        p1 = p2 + 2
-        cnt+=1
-        return [p2,p1,p3],cnt
-    elif d1>2 and d2>2:
-        if d1 > d2:
-            p3 = p1+2
-            cnt+=1
-            return [p1,p3,p2],cnt
-        else:
-            cnt+=1
-            p1 = p2+2
-            return [p2,p1,p3],cnt
+d1 = p2 - p1
+d2 = p3 - p2
 
-cnt = 0
-
-while 1:
-    pos.sort()
-    pos,cnt = comp(pos,cnt)
-    p1, p2, p3 = pos[0], pos[1], pos[2]
-    if p1==1 and p2 == 1 and p3 ==1:
-        break
-print(cnt)
+if d1 == 1 and d2 ==1:
+    sys.stdout.write(str(0))
+elif d1==2 or d2==2:
+    sys.stdout.write(str(1))
+else:
+    sys.stdout.write(str(2))
