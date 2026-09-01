b717cb3f666fba66035ded411272b39d147e4409
HappyPotatohappy <118255393+HappyPotatohappy@users.noreply.github.com>
2026-09-01 15:51:14 +0900

    [Passed] 가장 많이 나온 쌍 (52ms, 16MB)

--- trail2/가장 많이 나온 쌍/the-most-frequent-pair.py
+++ trail2/가장 많이 나온 쌍/the-most-frequent-pair.py
@@ -2,18 +2,20 @@ import sys
 input = sys.stdin.readline
 
 n, m = map(int, input().split())
-pairs = [list(map(int, input().split())) for _ in range(m)]
+pairs = [tuple(map(int, input().split())) for _ in range(m)]
 new = []
+cnt = {}
 for i in pairs:
     new.append(sorted(i))
+
 mx = -sys.maxsize
-for i in range(m):
-    tmp = 1
-    for j in range(m):
-        if i==j:
-            continue
-        if new[i] == new[j]:
-            tmp +=1
-    mx = max(mx,tmp)
+
+for i in new:
+    key = tuple(i)
+    if key not in cnt:
+        cnt[key] = 1
+    else:
+        cnt[key] +=1
+    mx = max(mx,cnt[key])
 
 sys.stdout.write(str(mx))
