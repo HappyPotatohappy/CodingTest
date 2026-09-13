e4b4684ca8719e8d9cdc6bae8b2b1641a466853c
HappyPotatohappy <118255393+HappyPotatohappy@users.noreply.github.com>
2026-09-13 09:32:18 +0900

    [Passed] 행복한 수열의 개수 (59ms, 17MB)

--- trail4/행복한 수열의 개수/number-of-happy-sequence.py
+++ trail4/행복한 수열의 개수/number-of-happy-sequence.py
@@ -1,26 +1,26 @@
 n, m = map(int, input().split())
 grid = [list(map(int, input().split())) for _ in range(n)]
 
-def find(num_list):
-    cnt_same = 0
-    for i in range(1,n):
-        if num_list[i] == num_list[i-1]:
-            cnt_same +=1
-            if cnt_same == m -1:
-                return 1
-        elif num_list[i] != num_list[i-1]:
-            cnt_same = 0
-    return 0
-        
 cnt = 0
+
+def check(numlist):
+    cnt = 0
+    for i in range(1,n):
+        if numlist[i-1] != numlist[i]:
+            cnt = 0
+            continue
+        if numlist[i-1] == numlist[i]:
+            cnt+=1
+        if cnt == m - 1:
+            return 1
+    if cnt >= m - 1:
+        return 1
+    return 0
+ans = 0
+
 for i in range(n):
-    tmp = grid[i]
-    tmp2 = [col[i] for col in grid]
-    if find(tmp):
-        cnt +=1
-    if find(tmp2):
-        cnt +=1
-if m==1:
-    print(2*n)
-else:
-    print(cnt)
+    row = grid[i]
+    col = [row[i] for row in grid]
+    ans += check(col)
+    ans += check(row)
+print(ans)
