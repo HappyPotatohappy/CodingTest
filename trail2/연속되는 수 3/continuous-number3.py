29dcb4333433ed21b6836888d0d44801d5555221
HappyPotatohappy <118255393+HappyPotatohappy@users.noreply.github.com>
2026-09-15 00:22:01 +0900

    [Passed] 연속되는 수 3 (73ms, 17MB)

--- trail2/연속되는 수 3/continuous-number3.py
+++ trail2/연속되는 수 3/continuous-number3.py
@@ -6,9 +6,6 @@ for i in range(1,n):
     if arr[i]*arr[i-1] > 0:
         cnt+=1
     else:
-        ans = max(ans,cnt)
         cnt = 1
-if cnt == n:
-    print(n)
-else:
-    print(ans)
+    ans = max(ans,cnt)
+print(ans)
