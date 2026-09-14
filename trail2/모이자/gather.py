dabd134bf9c9bf8f36d60d12c8a19e98adcd96bd
HappyPotatohappy <118255393+HappyPotatohappy@users.noreply.github.com>
2026-09-15 00:56:31 +0900

    [Passed] 모이자 (59ms, 17MB)

--- trail2/모이자/gather.py
+++ trail2/모이자/gather.py
@@ -1,22 +1,12 @@
-import sys
-input = sys.stdin.readline
-
-INT_MAX = sys.maxsize
-INT_MIN = -sys.maxsize
-
 n = int(input())
-h = list(map(int,input().split()))
-min_sum = INT_MAX
-
-for i in range(n):
-    now = i
+A = list(map(int, input().split()))
+A.insert(0,0)
+mn = 100*100*100
+for i in range(1,n+1):
     tmp = 0
-    for j in range(n):
-        if j == i:
+    for j in range(1,n+1):
+        if i==j:
             continue
-        leng = abs(i - j)
-        tmp += leng*h[j]
-    if tmp < min_sum:
-        min_sum = tmp
-
-sys.stdout.write(str(min_sum))
+        tmp += A[j]*(abs(i-j))
+    mn = min(tmp,mn)
+print(mn)
