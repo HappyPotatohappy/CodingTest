7ba7f952d4110ced778c3bb35dfd3c5f6e55be42
HappyPotatohappy <118255393+HappyPotatohappy@users.noreply.github.com>
2026-09-15 10:57:48 +0900

    [Passed] $1 \times 3$ 안의 최대 동전 수 (53ms, 16MB)

--- trail2/$1 \times 3$ 안의 최대 동전 수/best-place-of-13.py
+++ trail2/$1 \times 3$ 안의 최대 동전 수/best-place-of-13.py
@@ -1,17 +1,8 @@
-import sys
-input = sys.stdin.readline
-
 n = int(input())
 grid = [list(map(int, input().split())) for _ in range(n)]
-
-INT_MIN = -sys.maxsize
-mxnum = INT_MIN
+mx = 0
 for i in range(n):
-    tmp = 0
-    for j in range(n-2):
-        tmp = grid[i][j] + grid[i][j+1] + grid[i][j+2]
+    for j in range(n-3+1):
+        mx = max(mx,sum(grid[i][j:j+3]))
 
-        if tmp > mxnum:
-            mxnum = tmp
-
-sys.stdout.write(str(mxnum))
+print(mx)
