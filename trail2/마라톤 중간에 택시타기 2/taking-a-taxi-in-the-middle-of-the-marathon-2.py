6246ae8dc9e334958f7f45d366527c8eb1485bb6
HappyPotatohappy <118255393+HappyPotatohappy@users.noreply.github.com>
2026-09-15 11:18:03 +0900

    [Passed] 마라톤 중간에 택시타기 2 (56ms, 17MB)

--- trail2/마라톤 중간에 택시타기 2/taking-a-taxi-in-the-middle-of-the-marathon-2.py
+++ trail2/마라톤 중간에 택시타기 2/taking-a-taxi-in-the-middle-of-the-marathon-2.py
@@ -4,18 +4,14 @@ points = [tuple(map(int, input().split())) for _ in range(n)]
 x = [p[0] for p in points]
 y = [p[1] for p in points]
 
-def get_dist(p1,p2):
-    return abs(p1[0] - p2[0]) + abs(p1[1] - p2[1])
+mn = sys.maxsize
 
-ans = sys.maxsize
-for i in range(1,n-1):
+for i in range(1,n-1):## i는 뺼 점
     tmp = 0
-    for j in range(n-1):
-        if j == i-1:
-            tmp += get_dist([x[j],y[j]],[x[j+2],y[j+2]])
+    for j in range(1,n):
+        if i == j:
+            tmp += (abs(x[i-1]-x[j+1]) + abs(y[i-1] - y[j+1]) - abs(x[i]-x[i+1]) - abs(y[i]-y[i+1]))
             continue
-        if j == i:
-            continue
-        tmp += get_dist([x[j],y[j]],[x[j+1],y[j+1]])
-    ans = min(ans,tmp)
-print(ans)
+        tmp += (abs(x[j-1] - x[j]) + abs(y[j-1] - y[j]))
+    mn = min(tmp,mn)
+print(mn)
