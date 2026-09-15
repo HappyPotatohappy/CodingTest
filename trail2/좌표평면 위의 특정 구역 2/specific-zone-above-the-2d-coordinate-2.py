4af4e3d1bf1e0bebb7eb82c7efc0ef9e0553918d
HappyPotatohappy <118255393+HappyPotatohappy@users.noreply.github.com>
2026-09-15 16:32:42 +0900

    [Passed] 좌표평면 위의 특정 구역 2 (61ms, 17MB)

--- trail2/좌표평면 위의 특정 구역 2/specific-zone-above-the-2d-coordinate-2.py
+++ trail2/좌표평면 위의 특정 구역 2/specific-zone-above-the-2d-coordinate-2.py
@@ -1,25 +1,17 @@
 import sys
-input = sys.stdin.readline
-
 n = int(input())
 points = [tuple(map(int, input().split())) for _ in range(n)]
 x = [p[0] for p in points]
 y = [p[1] for p in points]
-
 mn = sys.maxsize
-
 for i in range(n):
-    xl = []
-    yl = []
-    for nx,ny in zip(x,y):
-        if nx == x[i] and ny == y[i]:
+    mx_x,mn_x,mx_y,mn_y = 0, sys.maxsize,0,sys.maxsize
+    for j in range(n):
+        if i == j:
             continue
-        xl.append(nx)
-        yl.append(ny)
-    if xl and yl:
-        s = (max(xl) - min(xl)) * (max(yl) - min(yl))
-    else:
-        s = 0
-    mn = min(s,mn)
-
-sys.stdout.write(str(mn))
+        mx_x = max(mx_x,x[j])
+        mn_x = min(mn_x,x[j])
+        mx_y = max(mx_y,y[j])
+        mn_y = min(mn_y,y[j])
+    mn = min((mx_x - mn_x)*(mx_y - mn_y),mn)
+print(mn)
