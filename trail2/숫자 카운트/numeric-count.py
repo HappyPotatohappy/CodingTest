42a0831f9bfd46ae5ef41637bdb70dff95a59f45
HappyPotatohappy <118255393+HappyPotatohappy@users.noreply.github.com>
2026-09-15 15:13:48 +0900

    [Passed] 숫자 카운트 (117ms, 20MB)

--- trail2/숫자 카운트/numeric-count.py
+++ trail2/숫자 카운트/numeric-count.py
@@ -1,39 +1,39 @@
 n = int(input())
 a, b, c = [], [], []
+
+def check(i,j,k,num):
+    num = list(map(int,str(num)))
+    cnt1 = 0
+    cnt2 = 0
+    cand = [i,j,k]
+    for l in range(3):
+        for m in range(3):
+            if l == m and num[l] == cand[m]:
+                cnt1+=1
+            if l!=m and num[l] == cand[m]:
+                cnt2+=1
+    return cnt1,cnt2
+                    
 for _ in range(n):
     num, cnt1, cnt2 = map(int, input().split())
     a.append(num)
     b.append(cnt1)
     c.append(cnt2)
 
-def comp(num1,num2):
-    l1 = list(str(num1))
-    l2 = list(str(num2))
-    cnt1 = 0
-    cnt2 = 0
-    for i in range(3):
-        if l1[i] == l2[i]:
-            cnt1+=1
-        elif l1[i] in l2:
-            cnt2+=1
-    return cnt1, cnt2
-
-num = list(range(1,10))
-cand =[]
-tlt = 0
+ans = 0
 for i in range(1,10):
     for j in range(1,10):
         for k in range(1,10):
-            if i==j or j==k or i==k:
+            if i==j or i==k or j==k:
                 continue
-            s = i*100+j*10+k
-            tmp = 0
-            for l in range(n):
-                cnt1, cnt2 = comp(s,a[l])
-                if cnt1 == b[l] and cnt2 == c[l]:
-                    tmp += 1
-            if tmp == n:
-                tlt += 1
-print(tlt)
-
-
+            cnt = 0
+            for s in range(n):
+                l = a[s]
+                p = b[s]
+                q = c[s]
+                cnt1, cnt2 = check(i,j,k,l)
+                if cnt1 == p and cnt2 == q:
+                    cnt+=1
+            if cnt == n:
+                ans +=1
+print(ans)
