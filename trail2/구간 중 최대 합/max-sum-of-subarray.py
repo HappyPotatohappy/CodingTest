1fc7ea0aade2979e49a2cdafa74c7f4a0cd5dd33
HappyPotatohappy <118255393+HappyPotatohappy@users.noreply.github.com>
2026-09-15 11:29:54 +0900

    [Passed] 구간 중 최대 합 (51ms, 16MB)

--- trail2/구간 중 최대 합/max-sum-of-subarray.py
+++ trail2/구간 중 최대 합/max-sum-of-subarray.py
@@ -1,13 +1,9 @@
-import sys
-input = sys.stdin.readline
-
-mx = -sys.maxsize
 n, k = map(int, input().split())
 arr = list(map(int, input().split()))
 
-
-
+mx = 0
 for i in range(n-k+1):
     mx = max(mx,sum(arr[i:i+k]))
 
-sys.stdout.write(str(mx))
+print(mx)
+
