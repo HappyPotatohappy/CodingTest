b52583d1093a18323f9654404a65a23848ba1566
HappyPotatohappy <118255393+HappyPotatohappy@users.noreply.github.com>
2026-09-21 10:14:10 +0900

    [Passed] 흰검 칠하기 (64ms, 21MB)

--- trail2/흰검 칠하기/painting-white-black.py
+++ trail2/흰검 칠하기/painting-white-black.py
@@ -1,44 +1,51 @@
-import sys
-input = sys.stdin.readline
-
 n = int(input())
 commands = [tuple(input().split()) for _ in range(n)]
 x = []
 dir = []
+mx_num = 100*1000
+vw = [0]*(2*mx_num)
+vb = [0]*(2*mx_num)
+ans =['']*(2*mx_num)
+
+
+start = mx_num
 for num, direction in commands:
     x.append(int(num))
     dir.append(direction)
-col_list = [[0,0,0] for _ in range(100*1000*2+1)]
-now = 100*1000
-
-for xi,di in zip(x,dir):
-    if di == "R":
-        st = now
-        ed = now + xi -1
-        now = ed
-        col = "b"
-        cnt_idx = 1
-    else:
-        st = now - xi + 1
-        ed = now
-        now = st
+    if direction == "L":
+        d = -1
         col = "w"
-        cnt_idx = 2
+        st = start + d*int(num) + 1
+        ed = start
+    else:
+        d = 1
+        col = "b"
+        st = start
+        ed = start + d*int(num) - 1
+
     for i in range(st,ed+1):
-        if col_list[i][0] != "g":
-            col_list[i][0] = col
-            col_list[i][cnt_idx]+=1
-            if col_list[i][1] >=2 and col_list[i][2] >=2:
-                col_list[i][0]="g"
-        
-cnt_b = 0
+        if ans[i] == "g":
+            continue
+        ans[i] = col
+        if col == "w":
+            vw[i] +=1
+        elif col == "b":
+            vb[i] +=1
+        if vw[i] >= 2 and vb[i] >= 2:
+            ans[i] = "g"
+    if col == "w":
+        start = st
+    else:
+        start = ed
 cnt_w = 0
+cnt_b = 0
 cnt_g = 0
-for i in range(100*1000*2+1):
-    if col_list[i][0] == "b":
-        cnt_b+=1
-    elif col_list[i][0] == "w":
-        cnt_w+=1
-    elif col_list[i][0] == "g":
+
+for i in ans:
+    if i == "w":
+        cnt_w +=1
+    elif i == "b":
+        cnt_b +=1
+    elif i =="g":
         cnt_g+=1
-sys.stdout.write(str(cnt_w) + " " + str(cnt_b) + " "+ str(cnt_g))
+print(cnt_w,cnt_b,cnt_g)
