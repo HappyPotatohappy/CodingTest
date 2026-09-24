68ee3739d52a69396a66ed1b8d90009acee71e2c
HappyPotatohappy <118255393+HappyPotatohappy@users.noreply.github.com>
2026-09-24 16:47:08 +0900

    [Passed] 야바위 (57ms, 17MB)

--- trail2/야바위/ya-rock.py
+++ trail2/야바위/ya-rock.py
@@ -1,19 +1,17 @@
-import sys
-input = sys.stdin.readline
-
+import random
 n = int(input())
 moves = [tuple(map(int, input().split())) for _ in range(n)]
 a, b, c = zip(*moves)
 a, b, c = list(a), list(b), list(c)
-mx = -sys.maxsize
-for i in range(1,4):
-    cup = [0]*(4)
+sc = 0
+for i in range(1,4): ## 어디에 돌을 넣을지 모두 탐색
+    cnt = 0
+    cup = [0]*4
     cup[i] = 1
-    sc = 0
-    for ai,bi,ci in zip(a,b,c):
-        cup[ai], cup[bi] = cup[bi], cup[ai]
-        if cup[ci] == 1:
-            sc +=1
-    mx = max(sc,mx)
+    for j in range(n):
+        cup[a[j]], cup[b[j]] = cup[b[j]], cup[a[j]]
+        if cup[c[j]] == 1:
+            cnt +=1
+    sc = max(cnt,sc)
 
-sys.stdout.write(str(mx))
+print(sc)
