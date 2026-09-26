0a0235efb977e5d75ac5c60bcee0ba3591e37b71
HappyPotatohappy <118255393+HappyPotatohappy@users.noreply.github.com>
2026-09-26 12:22:32 +0900

    [Passed] 두 직사각형 (46ms, 16MB)

--- trail2/두 직사각형/two-rectangles.py
+++ trail2/두 직사각형/two-rectangles.py
@@ -1,10 +1,36 @@
-import sys
-input = sys.stdin.readline
-
 x1, y1, x2, y2 = map(int, input().split())
 a1, b1, a2, b2 = map(int, input().split())
 
-if x2 < a1 or a2 < x1 or y2 < b1 or b2 < y1:
-    sys.stdout.write("nonoverlapping")
+def cover(r1,r2):
+    x1, y1, x2, y2 = r1[0], r1[1], r1[2], r1[3]
+    a1, b1, a2, b2 = r2[0], r2[1], r2[2], r2[3]
+
+    if x1 <= a1 <= x2 and y1 <= b1 <= y2:
+        return 1
+    if x1 <= a2 <= x2 and y1 <= b2 <= y2:
+        return 1
+    if x1 <= a1 <= x2 and y1 <= b2 <= y2:
+        return 1
+    if x1 <= a2 <= x2 and y1 <= b1 <= y2:
+        return 1
+    if a1 <= x1 <= a2 and b1 <= y1 <= b2:
+        return 1
+    if a1 <= x2 <= a2 and b1 <= y2 <= b2:
+        return 1
+    if a1 <= x1 <= a2 and b1 <= y2 <= b2:
+        return 1
+    if a1 <= x2 <= a2 and b1 <= y1 <= b2:
+        return 1 
+    if x1 <= a1 <= x2 and b1 <= y1 <= b2:
+        return 1 
+    if a1 <= x1 <= a2 and y1 <= b1 <= y2:
+        return 1 
+    return 0
+
+ans = cover([x1,y1,x2,y2],[a1,b1,a2,b2])
+if ans:
+    print("overlapping")
 else:
-    sys.stdout.write("overlapping")
+    print("nonoverlapping")   
+
+
